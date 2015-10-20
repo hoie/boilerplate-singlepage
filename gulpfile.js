ba0bca687210b22137effeffa826ac0f05ecd3e8
Oyvind Hoie <hoie@draft.no>
2015-10-20 12:41:08 +0200

    Cleaned up gulp file. Added Draft icons.

--- gulpfile.js
+++ gulpfile.js
@@ -22,94 +22,135 @@ var options = minimist(process.argv);
 var environment = options.environment || 'development';
 
 
-
-gulp.task('html', function(){
-	return gulp
-	.src('*.{html,ico,png}')
-	.pipe(gulp.dest('dist'))
-	.pipe(reload());
-});
-
-gulp.task('assets', function(){
-	return gulp
-	.src('assets/**/*')
-	.pipe(gulp.dest('dist/assets'))
-	.pipe(reload());
-});
-
-gulp.task('images', function(){
-	return gulp
-	.src('img/**/*.{jpg,png}')
-	.pipe(environment === 'production' ? imagemin() : gutil.noop())
-	.pipe(gulp.dest('dist/img'))
-	.pipe(reload());
-});
-
-gulp.task('sass', function(){
-	return gulp
-	.src('scss/styles.scss')
-	.pipe(environment === 'development' ? sourcemaps.init() : gutil.noop())
-	.pipe(sass({
-		includePaths:['scss/vendor/', 'node_modules/bootstrap-sass/assets/stylesheets/']
-	})).on('error', handleError)
-	//// Sourcecomments i stedet for sourcemap. Fjern linjen med sourcemaps.init...
-	// .pipe(sass({
-	// 	sourceComments: environment === 'development' ? 'map' : false
-	// })).on('error', handleError)
-	.pipe(postcss([ autoprefixer({ browsers: ['last 2 version'] }) ]))
-	.pipe(environment === 'production' ? minifyCss() : gutil.noop())
-	.pipe(sourcemaps.write())
-	.pipe(gulp.dest('dist/css'))
-	.pipe(reload());
-});
+/**
+ * COPY FILES TO DIST FOLDER
+ */
+	gulp.task('copy-files', function(){
+		gulp.src('*.{html,ico,png}')
+			.pipe(gulp.dest('dist'))
+			.pipe(reload());
+	});
 
 
-gulp.task('scripts', function(){
-	return browserify('./scripts/main.js', {
-		debug: environment === 'development'
-	})
-	.bundle().on('error', handleError)
-	.pipe(source('bundle.js'))
-	.pipe(environment === 'production' ? buffer() : gutil.noop())
-	.pipe(environment === 'production' ? uglify() : gutil.noop())
-	.pipe(gulp.dest('dist/scripts'))
-	.pipe(reload());
-});
+/**
+ * COPY ASSETS TO DIST/ASSETS FOLDER
+ */
+	gulp.task('copy-assets', function(){
+		gulp.src('assets/**/*')
+			.pipe(gulp.dest('dist/assets'))
+			.pipe(reload());
+	});
 
-// Clean out dist directory prior to new build
-// May be automated in build task in gulp 4 with gulp.series
-gulp.task('clean', function(){
-	del('dist/**/*')
-});
 
-gulp.task('server', function(){
-	server = express();
-	server.use(express.static('dist'));
-	server.listen(8000);
-	// browserSync({ proxy: 'localhost:8000', browser: "google chrome" });
-	browserSync({ proxy: 'localhost:8000' });
-});
+/**
+ * OPTIMIZE IMAGES TO DIST/IMG FOLDER
+ */
+	gulp.task('optimize-images', function(){
+		gulp.src('img/**/*.{jpg,png}')
+			.pipe(environment === 'production' ? imagemin() : gutil.noop())
+			.pipe(gulp.dest('dist/img'))
+			.pipe(reload());
+	});
 
-gulp.task('watch', function(){
-	gulp.watch('*.html', ['html']);
-	gulp.watch('assets/**/*', ['assets']);
-	gulp.watch('img/**/*[jpg,png]', ['images']);
-	gulp.watch('scss/**/*.scss', ['sass']);
-	gulp.watch('scripts/main.js', ['scripts']);
-});
 
-gulp.task('build', ['html', 'assets', 'images', 'sass', 'scripts']);
+/**
+ * COMPILE AND OPTIONALLY MINIFY SASS TO DIST/CSS FOLDER
+ */
+	gulp.task('compile-sass', function(){
+		gulp.src('scss/styles.scss')
+			.pipe(environment === 'development' ? sourcemaps.init() : gutil.noop())
+			.pipe(sass({
+				includePaths:['scss/vendor/', 'node_modules/bootstrap-sass/assets/stylesheets/']
+			})).on('error', handleError)
+			//// Sourcecomments i stedet for sourcemap. Fjern linjen med sourcemaps.init...
+			// .pipe(sass({
+			// 	sourceComments: environment === 'development' ? 'map' : false
+			// })).on('error', handleError)
+			.pipe(postcss([ autoprefixer({ browsers: ['last 2 version'] }) ]))
+			.pipe(environment === 'production' ? minifyCss() : gutil.noop())
+			.pipe(sourcemaps.write())
+			.pipe(gulp.dest('dist/css'))
+			.pipe(reload());
+	});
 
-gulp.task('default', ['build', 'watch', 'server']);
 
-function handleError(err) {
-	console.log(err.toString());
-	this.emit('end');
-};
+/**
+ * CONCAT AND OPTIONALLY MINIFY JS FILES WITH BROWSERIFY TO DIST/SCRIPTS FORLDER
+ */
 
-function reload() {
-	if (server) {
-		return browserSync.reload({ stream: true});
+	gulp.task('scripts', function(){
+		return browserify('./scripts/main.js', {debug: environment === 'development'})
+			.bundle().on('error', handleError)
+			.pipe(source('bundle.js'))
+			.pipe(environment === 'production' ? buffer() : gutil.noop())
+			.pipe(environment === 'production' ? uglify() : gutil.noop())
+			.pipe(gulp.dest('dist/scripts'))
+			.pipe(reload());
+	});
+
+
+/**
+ * CLEAN OUT DIST FOLDER PRIOR TO NEW BUILD
+ * May be automated in build task in gulp 4 with gulp.series
+ */
+	gulp.task('clean', function(){
+		del('dist/**/*')
+	});
+
+/**
+ * START SERVER AND BROWSERSYNC
+ */
+	gulp.task('server', function(){
+		server = express();
+		server.use(express.static('dist'));
+		server.listen(8000);
+		// browserSync({ proxy: 'localhost:8000', browser: "google chrome" });
+		browserSync({ proxy: 'localhost:8000' });
+	});
+
+
+/**
+ * WATCH CHANGES
+ */
+	gulp.task('watch', function(){
+		gulp.watch('*.html', ['copy-files']);
+		gulp.watch('assets/**/*', ['copy-assets']);
+		gulp.watch('img/**/*[jpg,png]', ['optimize-images']);
+		gulp.watch('scss/**/*.scss', ['compile-sass']);
+		gulp.watch('scripts/main.js', ['scripts']);
+	});
+
+
+/**
+ * BUILD PROJECT
+ */
+	gulp.task('build', ['copy-files', 'copy-assets', 'optimize-images', 'compile-sass', 'scripts']);
+
+
+/**
+ * DEFAULT TASK
+ * 
+ * Use 
+ * gulp --environment produdtion   
+ * to build optimized production version
+ * 
+ */
+	gulp.task('default', ['build', 'watch', 'server']);
+
+
+
+/**
+ * HELPER FUNCTIONS
+ */
+
+	function handleError(err) {
+		console.log(err.toString());
+		this.emit('end');
+	};
+
+	function reload() {
+		if (server) {
+			return browserSync.reload({ stream: true});
+		}
+		return gutil.noop();
 	}
-	return gutil.noop();
-}
